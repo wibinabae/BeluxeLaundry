a6229a5f50db97d373fbf08735d16c4292d8fcda
Neno Arisma <nenoarisma@gmail.com>
2023-06-25 11:59:06 +0700

    update

--- app/src/main/java/com/example/beluxe/Person.java
+++ app/src/main/java/com/example/beluxe/Person.java
@@ -1,13 +1,27 @@
 package com.example.beluxe;
 
 public class Person {
+
+
+    private String id;
     private String namana;
     private String kiloan;
+    private String harga;
 
-    public Person(String nama, String kilo) {
+    public Person() {}
 
+    public Person(String id, String nama, String kilo, String harga) {
+        this.id = id;
         this.namana = nama;
         this.kiloan = kilo;
+        this.harga = harga;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getNamana() {
@@ -25,6 +39,10 @@ public class Person {
     public void setKilo(String kilo) {
         this.kiloan = kilo;
     }
+
+    public String getHarga(){ return harga;};
+
+    public void setHarga(String harga) { this.harga = harga; }
 
 
 }
